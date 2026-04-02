ac0b21504b6a88d9e7c2d1ab5f33ea9081f2e346
Abijith K <79492151+mrastrange@users.noreply.github.com>
2026-04-02 09:25:03 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'https://faculty-activity-tracker.onrender.com/api/v1' || 'http://localhost:5000/api/v1',
 });
 
 // Add a request interceptor to append JWT token
